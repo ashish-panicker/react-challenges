60acf7c4b04feef755c19efb9a792deb3648ce5f
ashish-panicker <ashish.s.panicker@proton.me>
2026-02-05 16:45:45 +0530

    Reacts props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Profile from './components/Profile'
+
+const profile = {
+	name: 'John Smith',
+	email: 'john.smith@gmail.com',
+	role: 'Frontend Developer',
+	skills: ['react', 'css', 'javascript', 'figma'],
+	email: 'john@email.com',
+	city: 'New York'
+}
 
 function App() {
 	return (
 		<>
+		<Profile 
+			user={profile} text={""} />
 		</>
+
+		// props = {user: {}, text: ""}
+		// const {user} = props
 	)
 }
 
